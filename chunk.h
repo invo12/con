ef1f18b7b320a7490224fa872f683b9f395eca69
invo <alex_ilioi@yahoo.com>
2022-02-17 10:16:16 +0200

    Add strings

--- chunk.h
+++ chunk.h
@@ -7,8 +7,15 @@
 typedef enum {
     OP_ADD,
     OP_CONSTANT,
+    OP_NIL,
+    OP_TRUE,
+    OP_FALSE,
+    OP_EQUAL,
+    OP_GREATER,
+    OP_LESS,
     OP_DIVIDE,
     OP_MULTIPLY,
+    OP_NOT,
     OP_NEGATE,
     OP_RETURN,
     OP_SUBSTRACT
---
+++ object.h
@@ -0,0 +1,42 @@
+//
+// Created by invo on 2/17/2022.
+//
+
+#ifndef conq_object_h
+#define conq_object_h
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+
+typedef enum {
+    OBJ_STRING
+} ObjType;
+
+struct Obj {
+    ObjType type;
+    struct Obj* next;
+};
+
+struct ObjString {
+    Obj obj;
+    int length;
+    char *chars;
+};
+
+ObjString *copyString(const char *chars, int length);
+
+ObjString *takeString(char *chars, int length);
+
+void printObject(Value value);
+
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
