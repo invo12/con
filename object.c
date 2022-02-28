a05b33efbe3268f1841a2e02dab4274380f75107
invo <alex_ilioi@yahoo.com>
2022-02-28 05:33:57 +0200

    Add class declaration, instances, getter and setter

--- object.c
+++ object.c
@@ -23,6 +23,12 @@ static Obj *allocateObject(size_t size, ObjType type) {
     return object;
 }
 
+ObjClass *newClass(ObjString *name) {
+    ObjClass *klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+    klass->name = name;
+    return klass;
+}
+
 ObjUpvalue *newUpvalue(Value *slot) {
     ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
     upvalue->closed = NIL_VAL;
@@ -54,6 +60,14 @@ ObjFunction *newFunction() {
     function->name = NULL;
     initChunk(&function->chunk);
     return function;
+}
+
+ObjInstance *newInstance(ObjClass *klass) {
+
+    ObjInstance *instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
+    instance->klass = klass;
+    initTable(&instance->fields);
+    return instance;
 }
 
 ObjNative *newNative(NativeFn function) {
@@ -118,11 +132,18 @@ static void printFunction(ObjFunction *function) {
 
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
+        case OBJ_CLASS:
+            printf("%s", AS_CLASS(value)->name->chars);
+            break;
         case OBJ_CLOSURE:
             printFunction(AS_CLOSURE(value)->function);
             break;
         case OBJ_FUNCTION:
             printFunction(AS_FUNCTION(value));
+            break;
+        case OBJ_INSTANCE:
+            printf("%s instance",
+                   AS_INSTANCE(value)->klass->name->chars);
             break;
         case OBJ_NATIVE:
             printf("<native fn>");
--- object.h
+++ object.h
@@ -8,14 +8,21 @@
 #include "common.h"
 #include "value.h"
 #include "chunk.h"
+#include "table.h"
 
 #define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+
+#define IS_CLASS(value)        isObjType(value, OBJ_CLASS)
+#define AS_CLASS(value)        ((ObjClass*)AS_OBJ(value))
 
 #define IS_CLOSURE(value)      isObjType(value, OBJ_CLOSURE)
 #define AS_CLOSURE(value)      ((ObjClosure*)AS_OBJ(value))
 
 #define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
 #define AS_FUNCTION(value)     ((ObjFunction*)AS_OBJ(value))
+
+#define IS_INSTANCE(value)     isObjType(value, OBJ_INSTANCE)
+#define AS_INSTANCE(value)     ((ObjInstance*)AS_OBJ(value))
 
 #define IS_NATIVE(value)       isObjType(value, OBJ_NATIVE)
 #define AS_NATIVE(value) \
@@ -26,8 +33,10 @@
 #define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_CLASS,
     OBJ_CLOSURE,
     OBJ_FUNCTION,
+    OBJ_INSTANCE,
     OBJ_NATIVE,
     OBJ_STRING,
     OBJ_UPVALUE
@@ -69,6 +78,17 @@ typedef struct {
     int upvalueCount;
 } ObjClosure;
 
+typedef struct {
+    Obj obj;
+    struct ObjString *name;
+} ObjClass;
+
+typedef struct {
+    Obj obj;
+    ObjClass *klass;
+    Table fields;
+} ObjInstance;
+
 typedef Value (*NativeFn)(int argCount, Value *args);
 
 typedef struct {
@@ -80,9 +100,13 @@ ObjString *copyString(const char *chars, int length);
 
 ObjUpvalue *newUpvalue(Value *slot);
 
+ObjClass *newClass(ObjString *name);
+
 ObjClosure *newClosure(ObjFunction *function);
 
 ObjFunction *newFunction();
+
+ObjInstance* newInstance(ObjClass* klass);
 
 ObjNative *newNative(NativeFn function);
 
